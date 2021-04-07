f0b6bcdcc1d956c3681135e4b1ad399c16891f9d
anton264 <anton.eriksmo@gmail.com>
2021-04-07 14:13:26 +0200

    tjafs

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,21 +1,42 @@
-fun main(){
-    println("Hello world!")
-    val input = "FBFBBFFRLR"
-    var rows = input.subSequence(0,7)
-    var seats = input.subSequence(7,10)
-    var row = calc(rows, 0..127)
-    var seat = calc(seats, 0..7)
-    println(row*8+seat)
-
-}
-fun calc(input: CharSequence, numbers: IntRange): Int {
-    var seatRows = numbers.toList()
-    for (item in input){
-        when (item) {
-            'F','L' -> seatRows = seatRows.subList(0, seatRows.lastIndex/2+1)
-            'B','R' -> seatRows = seatRows.subList(seatRows.lastIndex/2+1, seatRows.lastIndex+1)
-        }
-        //println("${seatRows.first()} ${seatRows.last()}")
+fun main() {
+    val inputs = getResourceAsText("input")
+    var highestId = 0
+    val seatIds = mutableListOf<Int>()
+    for (input in inputs) {
+        val rows = input.subSequence(0, 7)
+        val seats = input.subSequence(7, 10)
+        val row = findRowOrColumn(rows, 0..127)
+        val seat = findRowOrColumn(seats, 0..7)
+        val seatId = row * 8 + seat
+        if (seatId > highestId) highestId = seatId
+        //Part 2
+        seatIds.add(seatId)
     }
-    return seatRows[0]
+    println("The highest seatId is: $highestId")
+    // Part 2
+    val sortedSeatIds = seatIds.sorted()
+    for ((index, seat) in sortedSeatIds.withIndex()) {
+        if (sortedSeatIds[index + 1] != seat + 1) {
+            println("My seat is ${seat + 1}")
+            break
+        }
+    }
+}
+
+fun findRowOrColumn(input: CharSequence, numbers: IntRange): Int {
+    var rowsOrColumns = numbers.toList()
+    for (item in input) {
+        when (item) {
+            'F', 'L' -> rowsOrColumns = rowsOrColumns.subList(0, rowsOrColumns.lastIndex / 2 + 1)
+            'B', 'R' -> rowsOrColumns = rowsOrColumns.subList(rowsOrColumns.lastIndex / 2 + 1, rowsOrColumns.lastIndex + 1)
+        }
+    }
+    return rowsOrColumns[0]
+}
+
+fun getResourceAsText(path: String): List<String> {
+    return object {}.javaClass
+        .getResource(path)
+        .readText()
+        .lines()
 }
